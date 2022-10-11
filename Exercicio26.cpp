71982de39a819a74ae56c14dc01c114111288bbf
Bruna Albuquerque <70643277+brunalbuq@users.noreply.github.com>
2022-10-11 11:26:03 -0300

    Create Exercicio26.cpp

---
+++ Exercicio26.cpp
@@ -0,0 +1,23 @@
+#include <GL/glut.h>
+
+void drawWindow()
+{
+ 5glClearColor(1.0f, 0.5f, 0.0f, 1.0f);
+ 6glClear(GL_COLOR_BUFFER_BIT);
+  
+ glBegin(GL_TRIANGLES);
+glVertex3f(0.0f, 0.5f, 0.0f);
+glVertex3f(-0.5f, -0.5f, 0.0f);
+glVertex3f(0.5f, -0.5f, 0.0f);
+glEnd();
+  
+glFlush();
+}
+ 
+int main(int argc, char *argv[])
+{
+glutCreateWindow("Olá Mundo OpenGL");
+glutDisplayFunc(drawWindow);
+glutMainLoop();
+return 1;
+  }
